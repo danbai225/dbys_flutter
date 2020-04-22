8b43a71601a5f91db5b134188b1b1358ae48744b
danbai225 <db225@qq.com>
2020-04-22 19:27:54 +0800

    投屏

--- lib/Page/ClassificationPage.dart
+++ lib/Page/ClassificationPage.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flustars/flustars.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
